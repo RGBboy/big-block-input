95881ba33dcf3a7ced36ea6831117b5ac0f02b3e
RGBboy <l-_-l@rgbboy.com>
2014-03-05 13:14:56 +1300

    Initial commit;

---
+++ index.js
@@ -0,0 +1,23 @@
+/*!
+ * Input System
+ */
+
+/**
+ * Module Dependencies
+ */
+
+
+
+/**
+ * Input System
+ *
+ * @return {InputSystem}
+ * @api public
+ */
+exports = module.exports = function () {
+
+  var self = {};
+
+  return self;
+
+};
---
+++ test/input.spec.js
@@ -0,0 +1,36 @@
+/*!
+ * Input unit tests
+ */
+
+/**
+ * Module Dependencies
+ */
+
+var test = require('tape'),
+    sinon = require('sinon'),
+    Input = require('../index'),
+    input;
+
+/**
+ * Setup
+ */
+
+var setup = function (t) {
+  input = Input();
+};
+
+/**
+ * Teardown
+ */
+
+var teardown = function (t) {
+};
+
+/**
+ * Input Class
+ */
+
+test('Input', function (t) {
+    t.plan(1);
+    t.ok(Input, 'class should exist');
+});
